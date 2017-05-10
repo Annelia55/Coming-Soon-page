780108ae41d1da4cf83e7a1122f600459956deae
Anna Bezrukova <annelia_55@mail.ru>
2017-05-10 19:15:52 +0700

    modal window accordion gallery done

--- js/index.js
+++ js/index.js
@@ -40,20 +40,47 @@ function upTime(countTo) {
 
 /* MODAL WINDOW - images */
 var modal = document.getElementById('myModal');
-
+var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
+var modal4 = document.getElementById('myModal4');
+var modal5 = document.getElementById('myModal5');
 
 var img = document.getElementById('myImg');
+var img2 = document.getElementById('myImg2');
+var img3 = document.getElementById('myImg3');
+var img4 = document.getElementById('myImg4');
+var img5 = document.getElementById('myImg5');
+
 var modalImg = document.getElementById("img01");
+var modalImg2 = document.getElementById("img02");
+var modalImg3 = document.getElementById("img03");
+var modalImg4 = document.getElementById("img04");
+var modalImg5 = document.getElementById("img05");
 
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
-}
+};
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+};
+img3.onclick = function(){
+    modal3.style.display = "block";
+    modalImg3.src = this.src;
+};
+img4.onclick = function(){
+    modal4.style.display = "block";
+    modalImg4.src = this.src;
+};
+img5.onclick = function(){
+    modal5.style.display = "block";
+    modalImg5.src = this.src;
+};
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
